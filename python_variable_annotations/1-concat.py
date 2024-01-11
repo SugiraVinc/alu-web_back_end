0a75d8bd6c2ccb443f8fac1bca1ed7a5c1163808
SugiraVinc <sugiravincentg11@gmail.com>
2024-01-11 17:49:47 +0200

    Float floor

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+""" To specify the data type/ annotation """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ return concantenated string """
+    return str1 + str2
---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+""" To specify the data type/ annotation """
+
+
+def floor(n: float) -> float:
+    """ Floating floor """
+    return n
