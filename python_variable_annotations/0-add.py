6fe37d42aeaeb7407c84594f6e4053775a583803
SugiraVinc <sugiravincentg11@gmail.com>
2024-01-11 17:09:07 +0200

    Annotation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 # using annotation to specify the data type.
-# We are adding two numbers to return addition
 
 
 def add(a: float, b: float) -> float:
+    # We are adding two numbers to return addition
+
     return a+b
