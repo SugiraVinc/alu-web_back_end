26139c6cc461f3e333b51ce389326489af30fac9
SugiraVinc <sugiravincentg11@gmail.com>
2024-01-11 17:16:07 +0200

    documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-# To specify the data type.
+# python3 -c 'print(__import__("my_module").__doc__)'
+# python3 -c 'print(__import__("my_module").MyClass.__doc__)'
 
 
 def add(a: float, b: float) -> float:
