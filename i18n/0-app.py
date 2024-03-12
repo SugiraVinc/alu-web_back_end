3e5d922c444010f3d38257ddb201e86deed5d5d2
SugiraVinc <sugiravincentg11@gmail.com>
2024-03-12 19:12:00 +0200

    flask projects

---
+++ i18n/0-app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""
+Import the Flask class from the flask module
+"""
+from flask import Flask, render_template
+
+# an instance of the Flask class and assign a variable to be the main entry point of the application
+app = Flask(__name__)
+
+
+# Define a route: URL path that the app responds to
+@app.route('/')
+def index() -> str:
+    return render_template('0-index.html')
+
+
+"""Run the app"""
+
+
+if __name__ == '__main__': app.run(debug=True)
