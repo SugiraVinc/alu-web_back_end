a86e04d23db8f96c10494bec9eb435b12016ccdf
SugiraVinc <sugiravincentg11@gmail.com>
2024-03-12 20:14:24 +0200

    entire project

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -1,2 +1,42 @@
 #!/usr/bin/env python3
 
+"""
+Module that instantiate the Babel object
+in the app. store it on module-level"""
+
+# Import the Babel module from flask_babel
+from flask import Flask, render_template
+from flask_babel import Babel
+
+# an instance of the Babel class and assign
+app = Flask(__name__)
+
+# Store babel object in a variable named babel.
+babel = Babel(app)
+
+
+class Config:
+    """
+    A class Config  that has language attributes
+    equal to en or fr"""
+    # Define the languages needed
+    LANGUAGES = ['en', 'fr']
+    # Set default locale and timezones
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+@app.route('/')
+def index() -> str:
+    """The home/index page
+    """
+
+    return render_template('1-index.html')
+
+""" Run the flask app"""
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- i18n/2-app.py
+++ i18n/2-app.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3
 
 """
-This is a basic Flask application with
-internationalization support using Flask-Babel.
+This is a basic Flask application with internationalization
+support using Flask-Babel.
 """
 
-from flask import Flask, render_template, request
+from flask import Config, Flask, render_template, request
 from flask_babel import Babel
-
 
 app = Flask(__name__)
 
 
-# Instantiate Babel object
+# Store babel object in a variable named babel.
 babel = Babel(app)
 
 
 class Config:
-    """
-    Configuration class for the Flask app.
+    """" create a Config class with
+     the specified attributes.
     """
     # Define available languages
     LANGUAGES = ['en', 'fr']
@@ -27,8 +26,8 @@ class Config:
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-# Use Config as config for the Flask app
 app.config.from_object(Config)
+
 
 # Define get_locale function using babel.localeselector decorator
 @babel.localeselector
@@ -41,11 +40,13 @@ def get_locale():
 
 
 @app.route('/')
-def index():
+def index() -> str:
+    """The home/index page.
     """
-    Render the index template.
-    """
-    return render_template('3-index.html')
+    return render_template('2-index.html')
+
+
+"""run the flask app"""
 
 
 if __name__ == '__main__':
