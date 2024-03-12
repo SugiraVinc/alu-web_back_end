16e4d6461f256e035a2d210532bf4f55e5b8c168
SugiraVinc <sugiravincentg11@gmail.com>
2024-03-12 19:30:33 +0200

    Function index documentation

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
 """
-Flaask module to return string 
-"when accessing the root URL ('/').
+This is a basic Flask application that returns
+"Hello world" when accessing the root URL ('/').
 """
 
 # import flask module
 from flask import Flask, render_template
 
-# an instance of the Flask class and assign 
-#a variable to be the main entry point of the application
+# an instance of the Flask class and assign
+# a variable to be the main entry point of the application
 app = Flask(__name__)
 
 
@@ -22,5 +22,5 @@ def index() -> str:
 """Run the app"""
 
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     app.run(debug=True)
