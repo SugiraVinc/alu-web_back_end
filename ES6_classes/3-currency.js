b2ddad0ef15efeef2c050fb473135cb5cf8724c3
SugiraVinc <sugiravincentg11@gmail.com>
2024-04-21 11:56:21 +0200

    indentation

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -3,25 +3,25 @@ class Currency {
     this._code = code;
     this._name = name;
   }
-  
+
   // Getter and setter for 'code'
   get code() {
     return this._code;
   }
-  
+
   set code(newCode) {
     this._code = newCode;
   }
-  
+
   // Getter and setter for 'name'
   get name() {
     return this._name;
   }
-  
+
   set name(newName) {
     this._name = newName;
   }
-  
+
   // Method to display full currency format
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
