0f8f81d9cf7683e73e8536ee865fe0b45d19d244
SugiraVinc <sugiravincentg11@gmail.com>
2024-04-21 15:37:14 +0200

    identation

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,6 @@
-// a function getStudentIdsSum that returns 
+// a function getStudentIdsSum that returns
 //...the sum of all the student ids
-// It should accept a list of students 
+// It should accept a list of students
 //..(from getListStudents) as a parameter
 // You must use the reduce function on the array
 
