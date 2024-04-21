269db3a166c1eacefdce18da2adf778332e3de5a
SugiraVinc <sugiravincentg11@gmail.com>
2024-04-21 15:31:58 +0200

    mapping

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,8 +2,8 @@
 function getListStudentIds(arrayOfObjects) {
   if (!Array.isArray(arrayOfObjects)) {
     return [];
-    }
-    
-    return arrayOfObjects.map((student) => student.id);
+  }
+
+  return arrayOfObjects.map((student) => student.id);
 }
 export default getListStudentIds;
