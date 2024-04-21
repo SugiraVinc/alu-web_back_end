f2212b2da1ae4a984639880a94a33f418639f7cf
SugiraVinc <sugiravincentg11@gmail.com>
2024-04-21 15:49:16 +0200

    set data structure

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,5 +1,5 @@
-//  function hasValuesFromArray that returns a boolean 
-//..if all the elements in the array exist within the set
+//  function hasValuesFromArray that returns a boolean
+// ..if all the elements in the array exist within the set
 // accepts two arguments: a set (Set) and an array (Array)
 
 function hasValuesFromArray(set, array) {
