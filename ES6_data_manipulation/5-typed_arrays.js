300c6f74a5c98cb31daf77c0d0f99f91262b6072
SugiraVinc <sugiravincentg11@gmail.com>
2024-04-21 15:47:08 +0200

    type arrays

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,9 @@
 //  a function named createInt8TypedArray that returns a new ArrayBuffer with an Int8
-//.. value at a specific position
-// It should accept three arguments: length (Number), position (Number), and value (Number
-// If adding the value is not possible the error Position outside range should be thrown
+// ..value at a specific position
+// It should accept three arguments: length (Number), position (Number),
+// and value (Number
+// If adding the value is not possible the error Position outside
+// range should be thrown
 
 function createInt8TypedArray(length, position, value) {
   if (position < 0 || position >= length) {
