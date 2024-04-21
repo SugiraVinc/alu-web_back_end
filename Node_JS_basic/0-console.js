e6871ca585d565aed62f38d185c2d731f0077c28
SugiraVinc <sugiravincentg11@gmail.com>
2024-04-21 17:06:05 +0200

    trials

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,5 +1,3 @@
-//  create a function named displayMessage
-// that prints in STDOUT the string argument
 module.exports = function displayMessage(stdout) {
- console.log(stdout);
+  console.log(stdout);
 };
