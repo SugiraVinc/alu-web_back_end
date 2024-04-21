8bd0d72893ce1b02895f7734696f460af9583042
SugiraVinc <sugiravincentg11@gmail.com>
2024-04-21 13:04:47 +0200

    indent

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -4,13 +4,13 @@ export default class Car {
     this._motor = motor;
     this._color = color;
   }
-  
+
   cloneCar() {
     const origin = this;
     return Object.assign(Object.create(Object.getPrototypeOf(origin)), {
-      _brand: undefined,
-      _motor: undefined,
-      _color: undefined,
-      });
+    _brand: undefined,
+    _motor: undefined,
+    _color: undefined,
+    });
     }
 }
